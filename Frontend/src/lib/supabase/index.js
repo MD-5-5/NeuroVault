ec0c3e2c02171fa5c1c33a2e410e488330bd928d
MD-5-5 <devsrivastav64@gmail.com>
2026-03-26 22:43:41 +0530

    setup_using_react

--- Frontend/src/lib/supabase/index.js
+++
@@ -1,6 +0,0 @@
-export { supabase } from "./client";
-export { createServerClient } from "./server";
-export * from "./auth";
-export * from "./db";
-export * from "./storage";
-export * from "./realtime";
