2ced6f42dfb7649661dce52696df0afa8afa27b7
MD-5-5 <devsrivastav64@gmail.com>
2026-03-28 16:08:22 +0530

    setting_redis

---
+++ Backend/src/config/redis.js
@@ -0,0 +1,14 @@
+import { Redis } from 'ioredis'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const redis = new Redis({
+  host: process.env.REDIS_HOST || 'localhost',
+  port: process.env.REDIS_PORT || 6379,
+  maxRetriesPerRequest: null
+})
+
+redis.on('connect', () => console.log('✅ Redis connected'))
+redis.on('error', (err) => console.error('❌ Redis error:', err.message))
+
+export default redis
