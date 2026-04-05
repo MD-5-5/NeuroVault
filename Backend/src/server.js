69cf5170141696cf7d2feddb537cd9799831df29
MD-5-5 <devsrivastav64@gmail.com>
2026-04-05 23:37:44 +0530

    fix cors for production

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -14,7 +14,7 @@ app.use(cors({ origin: [
   'http://localhost:5174',
   'http://localhost:3000',
   "https://intelliseek.vercel.app",
-  "https://neuro-vault-swart.vercel.app/home",
+  "https://neuro-vault-swart.vercel.app",
 ]}))
 app.use(express.json({ limit: '20mb' }))
 app.use(express.urlencoded({ extended: true, limit: '20mb' }));
